7f3357c55ba88edcc403e6c7e1fe4e700ed9c514
Jensey <50929863+seyleigh@users.noreply.github.com>
2019-11-17 17:33:05 -0500

    changed jquery

--- models/burger.js
+++ models/burger.js
@@ -31,7 +31,7 @@ Burger.create = function (burger) {
 
 Burger.updateDevoured = function (burgerId) {
     return new Promise((resolve, reject) => {
-        orm.updateOne("burgers", "DEVOURED", true, "ID", burgerId).then(results => {
+        orm.updateOne("burgers", "devoured", true, "ID", burgerId).then(results => {
             resolve(results);
         }).catch(() => {
             reject("Could not update burger");
