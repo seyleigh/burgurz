e92a4892d2b3b1a6ca5cfcc1b62d3823ab2cb98f
Jensey <50929863+seyleigh@users.noreply.github.com>
2019-11-17 13:09:43 -0500

    testing

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        database: "burger_db",
-        user: "root",
+        host: "kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        database: "pbhid2ik7q7jd6i1",
+        user: "jo49mr3kb0js79rh",
         port: 3306,
-        password: "whoops" //insert password
+        password: "d6wqsbj0i357w7uj" //insert password
     });
 }
 
