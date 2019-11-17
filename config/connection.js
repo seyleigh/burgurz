daffe5af6da7943d1448b1c59c4405ec1bae68aa
Jensey <50929863+seyleigh@users.noreply.github.com>
2019-11-17 13:01:59 -0500

    what is going on

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
         database: "burger_db",
         user: "root",
         port: 3306,
-        password: "root" //insert password
+        password: "whoops" //insert password
     });
 }
 
